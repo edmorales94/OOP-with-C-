d70bd8b9947e6ed338e74a0f41d65366dc2496da
Edgar Morales <edmorales94@gmail.com>
2018-02-18 18:20:58 -0500

    Car can drive and consume gallons, return the gallons remaining in tank, and gallons can be added as well

--- homework1/problem3/Car.cpp
+++ homework1/problem3/Car.cpp
@@ -1,13 +1,69 @@
 #include <iostream>
 
+using namespace std;
+
 class Car{
+//---------- Variables -----------------------------------------------------------------------------------------------------------
+    private:
+        int fuelEfficiency;//miles per gallon
+        int fuelLevel;//gallons in the tank
+
+    public:
+//---------- Constructor ---------------------------------------------------------------------------------------------------------
+        Car(int fuelEfficiency){
+            setEfficiency(fuelEfficiency);
+            setFuelLevel(0);
+        }
+
+//---------- Setters -------------------------------------------------------------------------------------------------------------
+        void setEfficiency(int efficiency){
+            fuelEfficiency = efficiency;
+        }
+
+        void setFuelLevel(int fuel){
+            fuelLevel = fuel;
+        }
+
+//---------- Getters -------------------------------------------------------------------------------------------------------------
+        int getFuelEfficiency(){
+            return fuelEfficiency;
+        }
+
+        int getGas(){
+            return fuelLevel;
+        }
+
+//---------- addGas method -------------------------------------------------------------------------------------------------------
+        void addGas(int gallonsToAdd){
+            setFuelLevel(getGas()+gallonsToAdd);
+        }
+
+//---------- drive method --------------------------------------------------------------------------------------------------------
+        void drive(int milesTravelled){
+            int gallonsToRemove = (int)(milesTravelled/getFuelEfficiency());//get how many gallons were used to travel that distance
+            setFuelLevel(getGas()-gallonsToRemove);//update the level of gallons remaining
+            if(getGas()< 0){//make sure we don't get less than 0 gallons
+                setFuelLevel(0);
+            }
+        }
 
 };
 
-using namespace std;
+//---------- main method ---------------------------------------------------------------------------------------------------------
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main(){
+    Car ToyotaPrius(52);
+    cout << "You have : " << ToyotaPrius.getGas() << " gallons remaining" << endl;
+    ToyotaPrius.addGas(20);
+    cout << "You have : " << ToyotaPrius.getGas() << " gallons remaining" << endl;
+    ToyotaPrius.drive(120);
+    cout << "You have : " << ToyotaPrius.getGas() << " gallons remaining" << endl;
+    ToyotaPrius.drive(839);
+    cout << "You have : " << ToyotaPrius.getGas() << " gallons remaining" << endl;
+    ToyotaPrius.drive(156);
+    cout << "You have : " << ToyotaPrius.getGas() << " gallons remaining" << endl;
+    ToyotaPrius.addGas(28);
+    cout << "You have : " << ToyotaPrius.getGas() << " gallons remaining" << endl;
+
     return 0;
 }
