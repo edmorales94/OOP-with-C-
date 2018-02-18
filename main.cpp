bcd91a63511b15cc1202e13a013fe8be6f64ec35
Edgar Morales <edmorales94@gmail.com>
2018-02-17 19:56:42 -0500

    First hw question

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    if(22<23){
+        cout << "Edgar is awesome";
+    }
+    else if( 23 == 23){
+        cout << "something";
+    }
+    else{
+        cout << "nothing worked";
+    }
+
+    return 0;
+}
