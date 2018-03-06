a418f70282d6d729fc759f1d365024ac3c61adb9
Edgar Morales <edmorales94@gmail.com>
2018-03-06 17:11:39 -0500

    A fully functional queue that implementes a linked list

--- homework4/main.cpp
+++ homework4/main.cpp
@@ -22,18 +22,20 @@ class Queue{
             head = tail = NULL;
             queueSize = 0;
         }
-    //---------- prototyped methods --------------------------------------------------------------------------------------------
-       // bool empty();
-       // int size();
-       // T front();
-        //T back();
-        //void push(T value);
-       // T pop();
-       // ~Queue();
 
+    //---------- Destructor ----------------------------------------------------------------------------------------------------
+    ~Queue(){
+        while(head != NULL){
+            Node<T> *temp = head;//Save the current head
+            head = head->next;//move the current head to the next element, which becomes the new head
+            delete temp;//delete the former head
+        }
+        tail = NULL;
+        cout<<"The queue was destroyed"<<endl;
+    }
     //---------- empty method --------------------------------------------------------------------------------------------------
         bool empty(){
-            return head == NULL && tail == NULL;//returns true if the head and tail are empty
+            return head == NULL && tail == NULL;//returns 1(true) if the head and tail are empty
         }
 
     //---------- size method ---------------------------------------------------------------------------------------------------
@@ -42,14 +44,14 @@ class Queue{
                 queueSize = 0;//in that case, set the size to zero
             }
             cout<<"size of queue: "<<queueSize<<endl;
-            return queueSize;
+            return queueSize;//return the number of elements in the queue
         }
 
     //---------- front method --------------------------------------------------------------------------------------------------
         T front(){
-            if(head != NULL){
+            if(head != NULL){//if there's an element in the queue
                 cout<<"first element: "<<head->nodeValue<<endl;
-                return head -> nodeValue;
+                return head -> nodeValue;//return the first element
             }
             else{
                 cout <<"Queue is empty. There's nothing at the front"<<endl;
@@ -58,9 +60,9 @@ class Queue{
 
     //---------- back method ---------------------------------------------------------------------------------------------------
         T back(){
-            if(tail != NULL){
+            if(tail != NULL){//if there's an element in the queue
                 cout<<"Last element: " << tail ->nodeValue<<endl;
-                return tail -> nodeValue;
+                return tail -> nodeValue;//return the element at the end
             }
             else{
                 cout <<"Queue is empty. There's nothing at the back"<<endl;
@@ -69,54 +71,55 @@ class Queue{
 
     //---------- push method ---------------------------------------------------------------------------------------------------
         void push(T value){
-            if(head == NULL){
-                head = new Node<T>(value);
-                tail = head;
-                queueSize++;
+            if(head == NULL){//if the queue is empty
+                head = new Node<T>(value);//the inserted element will be the first element
+                tail = head;//tail is pointing at the same element
+                ++queueSize;//increase the queue's size
                 cout<<value<<" was successfully inserted at the front of the queue"<<endl;
             }
-            else{
-                tail->next = new Node<T>(value);
-                tail = tail ->next;
-                queueSize++;
+            else{//there are more than one element
+                tail->next = new Node<T>(value);//tail's next element will be the new value
+                tail = tail ->next;//tail is point now to the next element, which becomes now the tail
+                ++queueSize;//increase the queue's size
                 cout<<value<<" was successfully inserted at the end of the queue"<<endl;
             }
         }
 
     //---------- pop method ----------------------------------------------------------------------------------------------------
         T pop(){
-            if(head == NULL){
+            if(head == NULL){//if the queue is empty
                 cout <<"Queue is empty. Can't pop anything"<<endl;
                 return NULL;
             }
-            else if(head == tail){
-                T temp = head->nodeValue;
-                head = NULL;
-                tail = NULL;
-                --queueSize;
-                return temp;
+            else if(head == tail){//if there is just one element in the queue
+                T temp = head->nodeValue;//get that value
+                head = NULL;//reset the head
+                tail = NULL;//reset the tail
+                --queueSize;//decrease the size
+                return temp;//return the saved value
             }
-            else{
-                T value = head -> nodeValue;
-                head = head ->next;
-                --queueSize;
+            else{//there is more than one element
+                T value = head -> nodeValue;//save the head's value
+                head = head ->next;//move the head to its next element, which becomes the new head
+                --queueSize;//decrease the queue's size
                 cout<<"popping the first element: "<<value<<endl;
-                return value;
+                return value;//return the saved value
             }
         }
 
     //---------- display method ------------------------------------------------------------------------------------------------
         void display(){
-            Node<T> *temp = head;
+            Node<T> *temp = head;//get a copy of the head
             cout<<"Current elements in the queue: ";
-            while(temp != NULL){
-                cout<<temp -> nodeValue <<" ";
-                temp = temp ->next;
+            while(temp){//while the temp is not null
+                cout<<temp -> nodeValue <<" ";//get the current value
+                temp = temp ->next;//next will be the copy of the next value
             }
             cout<<endl;
         }
 
 };
+//---------- main method --------------------------------------------------------------------------------------------------------
 int main()
 {
     Queue<int> myQueue;
@@ -143,5 +146,6 @@ int main()
     myQueue.size();
     myQueue.front();
     myQueue.back();
+    cout << "is myQueue empty? : " << myQueue.empty()<<endl;
     return 0;
 }
