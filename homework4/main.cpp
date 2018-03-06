2afa11535dc742eeb34f17442eb2d63ce59b9d8e
Edgar Morales <edmorales94@gmail.com>
2018-03-06 14:24:34 -0500

    empty method will return true or false if the queue is empty

--- homework4/main.cpp
+++ homework4/main.cpp
@@ -16,10 +16,23 @@ class Queue{
     public:
         Node<T> *head, *tail;
         int queueSize;
-
+    //---------- Constructor ---------------------------------------------------------------------------------------------------
         Queue(){
             head = tail = Null;
             queueSize = 0;
+        }
+    //---------- prototyped methods --------------------------------------------------------------------------------------------
+        bool empty();
+        int size();
+        T front();
+        T back();
+        void push();
+        T pop();
+        ~Queue();
+
+    //---------- empty method --------------------------------------------------------------------------------------------------
+        void bool empty(){
+            return head == Null && tail == Null;//returns true if the head and tail are empty
         }
 };
 int main()
