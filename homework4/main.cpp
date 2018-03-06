1eb1c11fe899bc523ce2a2b4e73728c5c38bb758
Edgar Morales <edmorales94@gmail.com>
2018-03-06 16:48:24 -0500

    Commented the prototyped methods and included a library for the NULL

--- homework4/main.cpp
+++ homework4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstddef>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ class Node{
         T nodeValue;
         Node<T> *next;
 
-        Node(const T &item, Node<T> *ptr = Null):nodeValue(item),next(ptr){}
+        Node(const T &item, Node<T> *ptr = NULL):nodeValue(item),next(ptr){}
 };
 
 template<typename T>
@@ -18,21 +19,21 @@ class Queue{
         int queueSize;
     //---------- Constructor ---------------------------------------------------------------------------------------------------
         Queue(){
-            head = tail = Null;
+            head = tail = NULL;
             queueSize = 0;
         }
     //---------- prototyped methods --------------------------------------------------------------------------------------------
-        bool empty();
-        int size();
-        T front();
-        T back();
-        void push();
-        T pop();
+       // bool empty();
+       // int size();
+       // T front();
+        //T back();
+        //void push(T value);
+       // T pop();
        // ~Queue();
 
     //---------- empty method --------------------------------------------------------------------------------------------------
-        void bool empty(){
-            return head == Null && tail == Null;//returns true if the head and tail are empty
+        bool empty(){
+            return head == NULL && tail == NULL;//returns true if the head and tail are empty
         }
 
     //---------- size method ---------------------------------------------------------------------------------------------------
@@ -40,12 +41,14 @@ class Queue{
             if(queueSize < 0){//size can't be less than zero
                 queueSize = 0;//in that case, set the size to zero
             }
+            cout<<"size of queue: "<<queueSize<<endl;
             return queueSize;
         }
 
     //---------- front method --------------------------------------------------------------------------------------------------
         T front(){
-            if(head != Null){
+            if(head != NULL){
+                cout<<"first element: "<<head->nodeValue<<endl;
                 return head -> nodeValue;
             }
             else{
@@ -55,7 +58,8 @@ class Queue{
 
     //---------- back method ---------------------------------------------------------------------------------------------------
         T back(){
-            if(tail != Null){
+            if(tail != NULL){
+                cout<<"Last element: " << tail ->nodeValue<<endl;
                 return tail -> nodeValue;
             }
             else{
@@ -65,37 +69,79 @@ class Queue{
 
     //---------- push method ---------------------------------------------------------------------------------------------------
         void push(T value){
-            Node<T> *temp = new Node();
-            temp -> nodeValue = value;
-            temp ->next = Null;
-
-            if(head == Null){
-                head = tail = temp;
+            if(head == NULL){
+                head = new Node<T>(value);
+                tail = head;
                 queueSize++;
-                cout<<value<<" was successfully inserted at the front"<<endl;
+                cout<<value<<" was successfully inserted at the front of the queue"<<endl;
             }
             else{
-                tail ->next = temp;
-                tail = temp;
+                tail->next = new Node<T>(value);
+                tail = tail ->next;
                 queueSize++;
-                cout<<value<<" was successfully inserted at the end"<<endl;
+                cout<<value<<" was successfully inserted at the end of the queue"<<endl;
             }
         }
 
     //---------- pop method ----------------------------------------------------------------------------------------------------
         T pop(){
-            if(head == Null){
-                count <<"Queue is empty. Can't pop anything"<<endl;
-                return;
+            if(head == NULL){
+                cout <<"Queue is empty. Can't pop anything"<<endl;
+                return NULL;
             }
-            T value = head -> nodeValue;
-            head = head ->next;
-            queueSize--;
-            return value;
+            else if(head == tail){
+                T temp = head->nodeValue;
+                head = NULL;
+                tail = NULL;
+                --queueSize;
+                return temp;
+            }
+            else{
+                T value = head -> nodeValue;
+                head = head ->next;
+                --queueSize;
+                cout<<"popping the first element: "<<value<<endl;
+                return value;
+            }
         }
+
+    //---------- display method ------------------------------------------------------------------------------------------------
+        void display(){
+            Node<T> *temp = head;
+            cout<<"Current elements in the queue: ";
+            while(temp != NULL){
+                cout<<temp -> nodeValue <<" ";
+                temp = temp ->next;
+            }
+            cout<<endl;
+        }
+
 };
 int main()
 {
-    cout << "Hello world!" << endl;
+    Queue<int> myQueue;
+    myQueue.push(1);
+    myQueue.push(2);
+    myQueue.push(6);
+    myQueue.push(10);
+    myQueue.size();
+    myQueue.empty();
+    myQueue.display();
+    myQueue.front();
+    myQueue.back();
+    myQueue.pop();
+    myQueue.display();
+    myQueue.size();
+    myQueue.pop();
+    myQueue.display();
+    myQueue.pop();
+    myQueue.display();
+    myQueue.pop();
+    myQueue.display();
+    myQueue.pop();
+    myQueue.display();
+    myQueue.size();
+    myQueue.front();
+    myQueue.back();
     return 0;
 }
